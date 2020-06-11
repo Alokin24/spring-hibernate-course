dfe7de8c133d34b813aaa70f7dfed068bc6232ee
Nikola Anastasovski <Nikola.Anastasovski@netcetera.com>
2020-06-11 16:02:16 +0200

    Demo of hibernate delete

---
+++ hibernate-tutorial/src/main/java/com/jdbc/hibernate/demo/DeleteStudentDemo.java
@@ -0,0 +1,45 @@
+package com.jdbc.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.jdbc.entity.Student;
+
+public class DeleteStudentDemo {
+  
+  public static void main(String[] args) throws Exception {
+    
+    // create session factory
+    SessionFactory factory = new Configuration()
+        .configure()
+        .addAnnotatedClass(Student.class)
+        .buildSessionFactory();
+    
+    // get session from factory
+    Session session = factory.getCurrentSession();
+    
+    // begin transaction
+    session.beginTransaction();
+    
+    // delete
+    
+    /* Delete demo #1
+    int id = 2;
+    Student refStudent = session.get(Student.class, id);
+    session.delete(refStudent);
+    */
+    
+    // Delete demo #2
+    
+    session.createQuery("delete from Student s where s.id=5").executeUpdate();
+    
+    
+    // commit transaction
+    session.getTransaction().commit();
+    
+    // close factory
+    factory.close();
+  }
+
+}
